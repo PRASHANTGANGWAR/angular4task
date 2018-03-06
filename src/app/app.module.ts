74a84adb20e98329dfe9b18b3bf118bb4e9b0cf2
PRASHANTGANGWAR <prashant.enbake@gmail.com>
2018-03-06 17:23:22 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+/*import { RouterModule } from '@angular/router'; */
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  { path: '**', component: LoginComponent }
+ 
+  /*{ path: 'heroes', component: HeroListComponent, data: { title: 'Heroes List' } },*/
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+    {path: '/', component: LoginComponent },
+      {path: 'register', component: RegisterComponent },
+      {path: 'dashboard', component: DashboardComponent }*/
